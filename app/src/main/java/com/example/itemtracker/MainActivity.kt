8aaaae5dba397d1e6f3b5a2137ed6b192bbb11df
SLuo5991 <sluo5991@gmail.com>
2022-03-26 15:19:11 -0400

    Open camera when click "Take Picture"

--- app/src/main/java/com/example/itemtracker/MainActivity.kt
+++ app/src/main/java/com/example/itemtracker/MainActivity.kt
@@ -17,11 +17,15 @@ import com.parse.*
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034
+    val photoFileName = "photo.jpg"
+    var photoFile: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Submit post when submit button is clicked
         findViewById<Button>(R.id.btnSubmit).setOnClickListener {
             val description = findViewById<EditText>(R.id.description).text.toString()
             val lostlocation = findViewById<EditText>(R.id.lostlocation).text.toString()
@@ -30,10 +34,71 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.btnTakePicture).setOnClickListener {
-            // launch camera
+            // launch
+            onLaunchCamera()
         }
 
         queryPost()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                val takenImage = BitmapFactory.decodeFile(photoFile!!.absolutePath)
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                val ivPreview: ImageView = findViewById(R.id.imageView)
+                ivPreview.setImageBitmap(takenImage)
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    // Launch Camera
+    fun onLaunchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName)
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        if (photoFile != null) {
+            val fileProvider: Uri =
+                FileProvider.getUriForFile(this, "com.codepath.fileprovider.Item_Tracker", photoFile!!)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+            if (intent.resolveActivity(packageManager) != null) {
+                // Start the image capture intent to take photo
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
+            }
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    fun getPhotoFileUri(fileName: String): File {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        val mediaStorageDir =
+            File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG)
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "failed to create directory")
+        }
+
+        // Return the file target for the photo based on filename
+        return File(mediaStorageDir.path + File.separator + fileName)
     }
 
     fun submitPost(description: String, lostlocation : String, user: ParseUser) {
@@ -48,6 +113,7 @@ class MainActivity : AppCompatActivity() {
                 exception.printStackTrace()
                 Toast.makeText(this, "Error in saving post", Toast.LENGTH_SHORT).show()
             } else {
+                Toast.makeText(this, "Successfully saved post", Toast.LENGTH_SHORT).show()
                 Log.i(TAG, "Successfully saved post")
             }
         }
